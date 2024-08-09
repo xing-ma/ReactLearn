95e22d8b685fa04284d1112facf1fd1002ad266c
carl.m <carl.ma@sjfood.com>
2024-08-09 05:04:41 +0000

    optimize

--- log-react/src/features/sample/hooks/effect.jsx
+++ log-react/src/features/sample/hooks/effect.jsx
@@ -3,23 +3,20 @@ import { Button } from 'antd';
 
 export const Effect = () => {
     const [data1, setData1] = useState(0);
-    const [data2, setData2] = useState(0);
-
+    const [data2, setData2] = useState(0)
+;
     useEffect(() => {
         console.log("每次都会执行, value: ", data1);
     });
-
-    useEffect(() => {
-        console.log("加载组件才会执行");
-    }, []);
 
     useEffect(() => {
         console.log("值变了才会执行");
     }, [data2]);
 
     useEffect(()=>{
+        console.log("首次加载组件才会执行");
         return ()=>{
-            console.log("加载组件和卸载组件的时候执行");
+            console.log("卸载组件的时候执行");
         }
     },[]);
 
